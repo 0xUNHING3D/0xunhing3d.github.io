099b6783edb05e92e84ff0596c729339c3dc63b9
kareembahnasy <karleeed88@gmail.com>
2025-08-18 03:52:29 +0000

    reverted git adjusted testsh

--- tools/test.sh
+++ tools/test.sh
@@ -62,8 +62,10 @@ main() {
 
   # test
   bundle exec htmlproofer "$SITE_DIR" \
+    --allow-hash-href \
     --disable-external \
     --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+
 }
 
 while (($#)); do
